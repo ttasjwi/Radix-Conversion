6aa4dbc404b923d428e3df769ff1f407ae4c4aec
ttasjwi <ttasjwi920@gmail.com>
2022-01-03 20:39:06 +0900

    byteAdd 메서드 추가
    Adder 클래스에 byteAdd 메서드를 추가했다.
    바이트 단위의 덧셈을 온전히 수행할 수 있다.
    
    - byteAdd 메서드
      - 두 boolean 배열에 대하여 Add 덧셈을 수행한다.
        - 배열의 크기가 다를 경우 큰 배열에 맞게 배열의 크기를 통일하여 연산을 수행한다.
        - 맨 마지막 자리수에서 carry값이 true가 반환되면, 배열의 길이를 1 증가시켜, 맨 마지막 자리에 true를 입력하여 반환한다.

--- Adder.java
+++ Adder.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Adder {
 
     public static boolean[] halfAdd(boolean a, boolean b) {
@@ -22,6 +24,26 @@ public class Adder {
         boolean[] result = new boolean[2];
         result[0] = totalCarryOut;
         result[1] = totalSum;
+        return result;
+    }
+
+    public static boolean[] byteAdd(boolean[] byteA, boolean[] byteB) {
+        if (byteA.length != byteB.length) { // 바이트 자릿수 통일
+            int maxLength = Math.max(byteA.length, byteB.length);
+            byteA = Arrays.copyOf(byteA, maxLength);
+            byteB = Arrays.copyOf(byteB, maxLength);
+        }
+        boolean[] result = new boolean[byteA.length];
+        boolean carry = false;
+        for (int i=0; i<result.length; i++) {
+            boolean[] fullAddition = fullAdd(byteA[i], byteB[i], carry);
+            result[i] = fullAddition[1];
+            carry = fullAddition[0];
+        }
+        if (carry) { // 마지막 자리수에서 올림이 발생했을 때
+            result = Arrays.copyOf(result,result.length+1); // 한 자리 큰 배열 생성
+            result[result.length] = true;
+        }
         return result;
     }
 }
