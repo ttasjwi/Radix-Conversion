6293ede5beca6ff1d9e4ed19d6d2962c5e077822
ttasjwi <ttasjwi920@gmail.com>
2022-01-03 18:40:49 +0900

    fullAdd 메서드 추가
    Adder 클래스에 fullAdd 메서드를 추가했다.
    비트 단위의 덧셈을 온전히 수행할 수 있다.
    
    - fullAdd 메서드
      - 비트 두 개의 합과, 자리올림수 입력이 주어졌을 때 다음 결과를 배열에 저장해 반환한다.
        - carryOut(0번 인덱스) : 자리올림수
        - Sum(1번 인덱스) : 합(자리에 남는 수)

--- Adder.java
+++ Adder.java
@@ -11,4 +11,17 @@ public class Adder {
 
         return result;
     }
+
+    public static boolean[] fullAdd(boolean bitA, boolean bitB, boolean carryIn) {
+        boolean[] bitAddition = halfAdd(bitA, bitB);
+        boolean[] carryInAddition = halfAdd(bitAddition[1], carryIn);
+
+        boolean totalCarryOut = LogicGate.or(bitAddition[0], carryInAddition[0]); // carry(자리 올림)
+        boolean totalSum = carryInAddition[1]; // sum (합)
+
+        boolean[] result = new boolean[2];
+        result[0] = totalCarryOut;
+        result[1] = totalSum;
+        return result;
+    }
 }
